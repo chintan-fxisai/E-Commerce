74bb0b425d9f3caed7332475caa1cfbd2861f42b
chintan-fxisai <chintanradadiya@fxis.ai>
2025-04-30 17:56:38 +0530

    created models

---
+++ Backend/products/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Product, Category, subCategory, ProductImage
+
+# Register your models here.
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'subcategory', 'price', 'quantity', 'is_active', 'created_at']
+    list_filter = ['subcategory', 'is_active']
+    search_fields = ['name', 'subcategory__name']
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'created_at']
+    search_fields = ['name']
+
+@admin.register(subCategory)
+class SubCategoryAdmin(admin.ModelAdmin):
+    list_display = ['id','name', 'category', 'created_at']
+    list_filter = ['category']
+    search_fields = ['name', 'category__name']
+
+@admin.register(ProductImage)
+class ProductImageAdmin(admin.ModelAdmin):
+    list_display = ['product', 'created_at']
+    search_fields = ['product__name']
+
+
