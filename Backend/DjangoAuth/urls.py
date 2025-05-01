3cb2ce619fa209361aedcc6cba1589b45df9802c
chintan-fxisai <chintanradadiya@fxis.ai>
2025-05-01 15:32:22 +0530

    Urls for product table created

--- Backend/DjangoAuth/urls.py
+++ Backend/DjangoAuth/urls.py
@@ -8,5 +8,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/',include('account.urls')),
+    path('api/shop/',include('products.urls')),
 ] + static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
