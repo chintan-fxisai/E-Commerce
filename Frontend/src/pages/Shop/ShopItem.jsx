de979842f0ca9a207e03e21f02dbaee4d5361684
chintan-fxisai <chintanradadiya@fxis.ai>
2025-05-07 12:47:26 +0530

    Shop page UI Updated

--- Frontend/src/pages/Shop/ShopItem.jsx
+++ Frontend/src/pages/Shop/ShopItem.jsx
@@ -1,29 +1,93 @@
-import { Box, colors, Typography } from '@mui/material'
-import React from 'react'
+import { Box, Typography } from '@mui/material'
+import React, { useState } from 'react'
 import './Shop.css'
+import { Paper, IconButton } from '@mui/material'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function ShopItem({ Image, ItemName, ItemPrice, onClick }) {
+
+    const [isHovering, setIsHovering] = useState(false)
+
     return (
         <>
-            <Box onClick={onClick}>
-                <Box className="shop-img">
-                    <img src={Image} alt="img" />
-                </Box>
-                <Box>
-                    <Typography variant="body1" color="initial"
-                        sx={
-                            {
-                                fontSize: 18,
-                                fontWeight: 600,
-                                color: 'grey',
-                                mt: 3
-                            }
-                        }>{ItemName}</Typography>
-                    <Typography variant="body1" color="initial"
-                        sx={{ 
-                            fontSize: 20,
-                            fontWeight:600 }}>{ItemPrice}</Typography>
-                </Box>
+            <Box >
+
+                <Paper elevation={2} style={{
+                    border: '1px solid black',
+                    cursor: 'pointer',
+                }}
+
+                    onMouseEnter={() => { setIsHovering(true) }}
+                    onMouseLeave={() => { setIsHovering(false) }}
+
+                >
+                    <Box className="shop-img" onClick={onClick}
+                        sx={{
+                            overflow: 'hidden',
+                            maxHeight: '500px',
+                            maxWidth: '500px',
+                            width: '100%',
+                            height: '300px',
+                            borderBottom: '1px solid black',
+                            background: '#f5f5f5',
+                            display: 'flex',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                        }}>
+                        <img style={{
+                            width: '100%',
+                            height: '100%',
+                            objectFit: 'cover',
+                            objectPosition: 'center',
+                            transition: 'all 0.3s ease',
+                            minWidth: '100%',
+                            minHeight: '100%',
+                        }} src={Image} alt="img" />
+                    </Box>
+                    <Box sx={{
+                        paddingX: 1,
+                        overflow: "hidden",
+                        textWrap:'nowrap'
+                                            }}>
+                        <Typography variant="body1" color="initial"
+                            sx={
+                                {
+                                    fontSize: 14,
+                                    fontWeight: 600,
+                                    color: isHovering ? '#000' : 'grey',
+                                    transition: "0.3s all ease",
+                                    mt: 1,
+                                    minHeight: '2rem',
+                                }
+                            }>{ItemName}</Typography>
+                        <Box sx={{
+                            display: 'flex',
+                        }}>
+                            <Typography variant="body1" color="initial"
+                                sx={{
+                                    fontSize: 20,
+                                    fontWeight: 600,
+                                    flexGrow: 1,
+                                    transition: ".3s all ease",
+                                    color: isHovering ? '#dc3545' : ''
+                                }}>{ItemPrice}
+                            </Typography>
+
+                            <IconButton className={'show-cart'}
+                                aria-label="cart"
+                                disableRipple
+                                sx={{
+                                    display: isHovering ? 'block' : 'none',
+                                    padding: 0
+                                }}>
+                                <ShoppingCartIcon
+                                    sx={{
+                                        width: 30,
+                                    }} />
+                            </IconButton>
+                        </Box>
+                    </Box>
+                </Paper>
             </Box>
         </>
     )
